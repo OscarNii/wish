458780cfa9c17115966c430f9cc19b2b121b913d
OscarNii <Immanuelsowahoscar@gmail.com>
2023-04-16 19:53:14 +0000

    iosRunner commit

--- lib/gini.dart
+++ lib/gini.dart
@@ -1,10 +1,16 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-
 import 'GiniWish.dart';
 
-class MyGiniPage extends StatelessWidget {
+class MyGiniPage extends StatefulWidget {
   const MyGiniPage({super.key});
+
+  @override
+  State<MyGiniPage> createState() => _MyGiniPageState();
+}
+
+class _MyGiniPageState extends State<MyGiniPage> {
+  bool isAnimating = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,28 +19,47 @@ class MyGiniPage extends StatelessWidget {
       child: Column(
         children: [
           Center(
-            child: Container(
-              height: 330,
-              width: 300,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/ginis.png"),
-                  fit: BoxFit.fill,
+            child: FadeInDownBig(
+              child: Container(
+                height: 330,
+                width: 300,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/ginis.png"),
+                    fit: BoxFit.fill,
+                  ),
                 ),
               ),
             ),
           ),
-          SizedBox(height: 40),
+          const SizedBox(height: 40),
+          const Text(
+            "Abracadabra",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 40),
           SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Row(
               children: [
-                FadeInLeftBig(child: GiniWish()),
-                FadeInDown(child: GiniWish()),
-                FadeInRight(child: GiniWish()),
+                FadeInLeft(animate: isAnimating, child: const GiniWish()),
+                FadeInDown(animate: isAnimating, child: const GiniWish()),
+                FadeInRight(animate: isAnimating, child: const GiniWish()),
               ],
             ),
           ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                isAnimating = !isAnimating;
+              });
+            },
+            child: const Text(
+              "Make a Wish",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
+          )
         ],
       ),
     ));
